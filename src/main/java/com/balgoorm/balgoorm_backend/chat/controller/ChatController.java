29afa968beb232fae9ab42a0a62bf53fae79e1dc
승헌이 <seungheon-i@seungheon-iui-MacBookPro.local>
2024-06-20 15:31:35 +0900

    add /api mapping

--- src/main/java/com/balgoorm/balgoorm_backend/chat/controller/ChatController.java
+++ src/main/java/com/balgoorm/balgoorm_backend/chat/controller/ChatController.java
@@ -12,12 +12,14 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ChatController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
