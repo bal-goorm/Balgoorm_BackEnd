7ad979aa0f9f9902528647bb6e90213a04007498
cxzlkjhgfdsa <cxzlkjhgfdsa@naver.com>
2024-06-20 16:02:14 +0000

    fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 # Docker 데몬을 백그라운드에서 실행
-dockerd &
+dockerd --host=unix:///var/run/docker.sock &
 
-# Docker 데몬이 시작될 때까지 잠시 대기
-while(! docker info > /dev/null 2>&1); do
-    sleep 1
-done
+# Docker 데몬이 시작될 때까지 10초 대기
+sleep 10
 
 # 애플리케이션 실행
 java -jar /app/build/libs/Balgoorm_BackEnd-0.0.1-SNAPSHOT.jar
