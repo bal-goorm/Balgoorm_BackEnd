231e7c50f9226ecdad81f177ee9a0ffee4771224
승헌이 <seungheon-i@seungheon-iui-MacBookPro.local>
2024-06-21 12:06:31 +0900

    add test controller

---
+++ src/main/java/com/balgoorm/balgoorm_backend/quiz/controller/CustomController.java
@@ -0,0 +1,22 @@
+package com.balgoorm.balgoorm_backend.quiz.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class CustomController {
+
+
+    @GetMapping("/test")
+    public ResponseEntity test(){
+
+        log.info("test ok");
+
+        return ResponseEntity.ok("ok");
+    }
+}
